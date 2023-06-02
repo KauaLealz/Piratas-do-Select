0ef07b2629697712ea5dc5c639f14f1bca8a4d39
KauaLealz <130408422+KauaLealz@users.noreply.github.com>
2023-06-02 11:31:11 -0300

    Script dos Selects Tabelas

---
+++ script Selects.sql
@@ -0,0 +1,29 @@
+       INSERT INTO Pirata VALUES 
+       (1, 'Jack Sparrow', 'Espadachim, Navegador'),
+       (2, 'Barba Negra', 'Liderança, Manipulação'),
+       (3, 'Anne Bonny', 'Atiradora, Estrategista');
+       
+       INSERT INTO Tripulacao VALUES 
+       (1, 'Tripulação do Pérola Negra'),
+       (2, 'Tripulação do Vingança da Rainha'),
+       (3, 'Tripulação do Espírito Vingativo');
+       
+       INSERT INTO Navio VALUES 
+       (1, 'Pérola Negra', 'Navio de Guerra'),
+       (2, 'Vingança da Rainha', 'Galeão'),
+       (3, 'Espírito Vingativo', 'Fragata');
+       
+       INSERT INTO Tesouro VALUES 
+       (1, 1000000, 'Ilha da Caveira'),
+       (2, 500000, 'Ilha do Tesouro'),
+       (3, 2000000, 'Ilha dos Piratas');
+       
+       INSERT INTO Localizacao VALUES 
+       (1, 'Ilha da Caveira'),
+       (2, 'Ilha do Tesouro'),
+       (3, 'Ilha dos Piratas');
+       
+       INSERT INTO Alianca VALUES 
+       (1, 1, 2),
+       (2, 2, 3),
+       (3, 1, 3);
